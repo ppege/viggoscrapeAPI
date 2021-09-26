6e1f3ba17ffa38eb11d03f5507cc0239348cff5a
NanguRepo <nanguthenangu@gmail.com>
2021-09-26 18:39:58 +0200

    maybe heroku will work now

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+from app import routes
+
+
+app.run(debug=True)
---
+++ app/api.py
@@ -0,0 +1,32 @@
+from app import app
+from scraper import get_assignments
+
+@app.route('/', methods=['GET'])
+def home():
+    return app.render_template('landing.html')
+
+@app.route('/api/v1/scrape', methods=['GET'])
+def scrape():
+    error_list = []
+    if 'subdomain' not in app.request.args:
+        error_list.append("No subdomain provided")
+    if 'username' not in app.request.args:
+        error_list.append("No username provided")
+    if 'password' not in app.request.args:
+        error_list.append("No password provided")
+    if error_list != []:
+        errors = "<br>".join(error_list)
+        return f"Error(s) ocurred:<br>{errors}"
+
+    
+    return app.jsonify(
+        get_assignments(
+            app.request.args['subdomain'],
+            {
+                "USERNAME": app.request.args['username'],
+                "PASSWORD": app.request.args['password']
+            }
+        )
+    )
+
+app.run()
