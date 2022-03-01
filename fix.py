e747a8fbff523f65f26567aa4cfc61ab9b3add00
NanguRepo <nanguthenangu@gmail.com>
2022-03-01 22:18:16 +0100

    add exotic value

--- fix.py
+++ fix.py
@@ -3,18 +3,25 @@ import json
 with open("values.json", "r") as file:
   data = json.load(file)
 
-def sum_of_list(l):
-  total = 0
-  for val in l:
-    total = total + val
-  return total
-
 for key in data:
-  demand = data[key]["DEMAND"]
-  demand = demand.replace(u"\u2605", "1").replace(u"\u2606", "0").replace("NOT TRADABLE", "0").replace("???", "0")
-  demand = list(demand)
-  demand = [int(i) for i in demand]
-  data[key]["DEMANDNUMBER"] = sum_of_list(demand)
+  exotic_value = False
+  if "Legendar" in str(data[key]["VALUE"]):
+    exotic_value = int(data[key]["VALUE"][0])/6
+  if "Common" in str(data[key]["VALUE"]):
+    exotic_value = int(data[key]["VALUE"][0])/120
+  if "Rare" in str(data[key]["VALUE"]):
+    exotic_value = int(data[key]["VALUE"][0])/30
+  if "Exotic" in str(data[key]["VALUE"]):
+    exotic_value = int(data[key]["VALUE"][0])
+  if isinstance(data[key]["VALUE"], int):
+    exotic_value = int(data[key]["VALUE"])
 
-with open("values.json", "w") as file:
+  if not exotic_value:
+    try:
+      exotic_value = int(data[key]["VALUE"].replace(',', ''))
+    except:
+      exotic_value = "Unknown"
+  data[key]["EXOTICVALUE"] = exotic_value
+
+with open("values2.json", "w") as file:
   json.dump(data, file, indent=4)
