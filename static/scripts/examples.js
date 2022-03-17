867e58204a84a22d4c1bef4fd18914cdded32d2c
NanguRepo <nanguthenangu@gmail.com>
2022-03-17 21:44:48 +0100

    progress

--- static/scripts/examples.js
+++ static/scripts/examples.js
@@ -3,3 +3,19 @@ function goto(place) {
         scrollTop: $(`#${place}`).offset().top
     }, 500);
 }
+
+function insertGist(url, selector) {
+    fetch(url)
+    .then(response => response.text())
+    .then(text => {
+        marked.setOptions({
+            highlight: function(code) {
+            return hljs.highlightAuto(code).value;
+            }
+        });
+        $(selector).html(marked.parse(text));
+    });
+}
+
+insertGist('https://gist.githubusercontent.com/NanguRepo/7bec404ae851725b3abb7bdbec9fb88f/raw/', '#pythonBody');
+insertGist('https://gist.githubusercontent.com/NanguRepo/f139e748e13b497da2b84435848e0407/raw/', '#javascriptBody');
---
+++ static/scripts/externalLinks.js
@@ -0,0 +1,8 @@
+$.expr[':'].external = function(obj){
+    return !obj.href.match(/^mailto\:/)
+           && (obj.hostname != location.hostname)
+           && !obj.href.match(/^javascript\:/)
+           && !obj.href.match(/^$/)
+};
+
+$('a:external').attr('target', '_blank');
